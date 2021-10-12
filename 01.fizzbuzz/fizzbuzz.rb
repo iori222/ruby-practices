2035dc948461130aded64ed703b9c7152cd572ae
iori222 <kohira@catallaxy.me>
2021-10-13 00:21:51 +0900

    修正しました

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,29 +1,26 @@
-21.times do |x| 
-    x + 1
-
-if (x) % 15 == 0
-    puts "FizzBizz"
-elsif (x) % 5 == 0
-    puts "Buzz"
-elsif (x) % 3 == 0
-    puts "Fizz"
-else 
-    puts x;
-end
-
-end
-
-#こちらの方がスマートかなと思いました！
 (1..20).each do |x| 
-   
-if (x) % 15 == 0
-    puts "FizzBizz"
-elsif (x) % 5 == 0
-    puts "Buzz"
-elsif (x) % 3 == 0
-    puts "Fizz"
-else 
-    puts x;
+    if x % 15 == 0
+         puts "FizzBizz"
+     elsif x % 5 == 0
+         puts "Buzz"
+     elsif x % 3 == 0
+         puts "Fizz"
+     else 
+         puts x;
+     end
+ end
+
+
+20.times do |x| 
+    y = x + 1
+   if y % 15 == 0
+        puts "FizzBizz"
+    elsif y % 5 == 0
+        puts "Buzz"
+    elsif y % 3 == 0
+        puts "Fizz"
+    else 
+        puts y;
+    end
 end
 
-end
